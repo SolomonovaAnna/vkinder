27c114993c27e4d85974632f24e7738891c74ee7
SolomonovaAnna <131543003+SolomonovaAnna@users.noreply.github.com>
2023-04-24 12:15:25 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,19 +3,21 @@ from tools import bot
 from database import creating_database
 
 
+if __name__ == '__main__':
+    for event in bot.longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            res = event.text.lower()
+            user_id = event.user_id
+            if res == 'поиск':
+                # while True:
+                creating_database()
+                bot.get_age(user_id)
+                bot.get_city(user_id)
+                bot.get_sex(user_id)
+                bot.users_search(bot.move_offset())
+                bot.show_found_person(user_id)
+            else:
+                bot.send_msg(user_id, f' Бот готов к поиску, наберите: \n '
+                                      f' "Поиск - Поиск людей. \n'
+                                      )
 
-for event in bot.longpoll.listen():
-    if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-        res = event.text.lower()
-        user_id = event.user_id
-        if res == 'поиск':
-               creating_database() 
-               bot.get_age(user_id)
-               bot.get_city(user_id)
-               bot.get_sex(user_id)
-               bot.users_search(bot.move_offset())
-               bot.show_found_person(user_id)
-        else:
-            bot.send_msg(user_id, f' Бот готов к поиску, наберите: \n '
-                                  f' "Поиск - Поиск людей. \n'
-                                  )
